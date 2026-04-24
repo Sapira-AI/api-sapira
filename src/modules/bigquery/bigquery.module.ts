afe5415a753be29de62ae07ae3aa0b649a7fe2a0
León Montero <lrmontero@gmail.com>
2026-04-24 06:18:28 -0400

    bigquery clientes

--- src/modules/bigquery/bigquery.module.ts
+++ src/modules/bigquery/bigquery.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from '@/auth/auth.module';
 import { StripeCustomerBigQuery } from '@/databases/postgresql/entities/stripe-customer-bigquery.entity';
+import { CompanyHolding } from '@/modules/holdings/entities/company-holding.entity';
 
 import { BigQueryController } from './bigquery.controller';
 import { BigQueryProviders } from './bigquery.provider';
+import { BigQueryScheduler } from './bigquery.scheduler';
 import { BigQueryService } from './bigquery.service';
 
 @Module({
-	imports: [AuthModule, TypeOrmModule.forFeature([StripeCustomerBigQuery])],
+	imports: [AuthModule, ScheduleModule.forRoot(), TypeOrmModule.forFeature([StripeCustomerBigQuery, CompanyHolding])],
 	controllers: [BigQueryController],
-	providers: [BigQueryService, ...BigQueryProviders],
+	providers: [BigQueryService, BigQueryScheduler, ...BigQueryProviders],
 	exports: [BigQueryService],
 })
 export class BigQueryModule {}
